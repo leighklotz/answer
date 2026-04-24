1b30ee696441615e97216b0dda64641e4df9f08e
Leigh Klotz <klotz@klotz.me>
2026-04-24 12:10:05 -0700

    Update disable.sh and enable.sh scripts
    Add usage instructions to comments, add error handling for failed disables, and fix PS1 assignment logic.

--- disable.sh
+++ disable.sh
@@ -1,7 +1,8 @@
-# disable.sh
+# disable.sh: source this file
 export PATH="${PATH#$HOME/wip/answer:}"
 if [ -n "${ANSWER_OLD_PS1}" ]; then
     export PS1="${ANSWER_OLD_PS1}"
     unset ANSWER_OLD_PS1
+elif [[ "${PS1}" =~ "🦶" ]]; then
+    echo "🦶: failed to disable"
 fi
-
--- enable.sh
+++ enable.sh
@@ -1,4 +1,5 @@
+# enable.sh: source this file
 export PATH=~/wip/answer:"${PATH}"
-export ANSWER_OLD_PS1="🦶${PS1}"
-export PS1="${PS1}"
+export ANSWER_OLD_PS1="${PS1}"
+export PS1="🦶${PS1}"
 source ~/wip/answer/functions.sh
