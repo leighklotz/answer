5812cecc2d0db051ae20cc002e539f030f604841
Leigh Klotz <klotz@klotz.me>
2026-04-24 10:33:40 -0700

    Update condition in disable.sh to check if ANSWER_OLD_PS1 is non-empty

--- disable.sh
+++ disable.sh
@@ -1,6 +1,6 @@
 # disable.sh
 export PATH="${PATH#$HOME/wip/answer:}"
-if [ -z "${ANSWER_OLD_PS1}" ]; then
+if [ -n "${ANSWER_OLD_PS1}" ]; then
     export PS1="${ANSWER_OLD_PS1}"
     unset ANSWER_OLD_PS1
 fi
