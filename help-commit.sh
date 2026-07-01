98686a471f3d3939e6eadebdbabfbc79d32b0bc2
Leigh Klotz <klotz@klotz.me>
2026-07-01 15:19:26 -0700

    refactor(help-commit): split prompts and add dependency sourcing and logging

--- help-commit.sh
+++ help-commit.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
+source "${SCRIPT_DIR}/env.sh"
+source "${SCRIPT_DIR}/logging.sh"
 source "${SCRIPT_DIR}/functions.sh"
 
-GIT_COMMIT_PROMPT="Below is the output of a git bash session.  Read the session and then briefly output a code fence containing a corresponding \`git commit\` command, using using one or more bash git commands as appropriate for the change. Use conventional commits. For commits that are not single-focus, give more descriptive messages. If you do not have enough information to write a commit message and need to see more git results, output a brief request concluding with a code fence containing one or more bash git commands to execute to obtain the results. \n"
+GIT_COMMIT_PROMPT="Below is the output of a git bash session.  Read the session and then briefly output a code fence containing a corresponding \`git commit\` command, using using one or more bash git commands as appropriate for the change. Use conventional commits. For commits that are not single-focus, give more descriptive messages.\n"
+
+GIT_COMMIT_TOOL_PROMPT="Below is the output of a git bash session.  Read the session and then briefly output a code fence containing a corresponding \`git commit\` command, using using one or more bash git commands as appropriate for the change. Use conventional commits. For commits that are not single-focus, give more descriptive messages. If you do not have enough information to write a commit message and need to see more git results, output a brief request concluding with a code fence containing one or more bash git commands to execute to obtain the results. \n"
 
 # TODO pipetest needs to present only the human-readable last conversion but send on the whole convo
 # or should it take only code? or should we skill unfence to prompt instead that?
 # `pipetest | answer` or `answer | pipetest` or `unfence --ask`
 # much confusion about whether each these should accept/produce json or text
-
-# (bx git status; bx git diff --numstat; bx git diff; git diff --cached) | ask -i "${GIT_COMMIT_PROMPT}" |  pipetest | answer | unfence | bash
-# (bx git status; bx git diff --numstat; bx git diff; git diff --cached) | ask -i "${GIT_COMMIT_PROMPT}" |  answer | pipetest | unfence | bash
-# (bx git status; bx git diff --numstat; bx git diff; git diff --cached) | ask -i "${GIT_COMMIT_PROMPT}" |  answer | unfence --ask | bash
 
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo "🦶$(basename "$0"): PWD=$PWD is not in a git repository"
@@ -51,6 +51,8 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+log_warn "ASK_OPTIONS=$ASK_OPTIONS"
+log_warn "GIT_DIFF_OPTIONS=$GIT_DIFF_OPTIONS"
 
 (bx pwd; bx git rev-parse --show-toplevel; bx git status; bx git diff --numstat ${GIT_DIFF_OPTIONS}; bx git diff ${GIT_DIFF_OPTIONS}; bx git diff --cached ${GIT_DIFF_OPTIONS}) \
   | ask -i "${GIT_COMMIT_PROMPT}" ${ASK_OPTIONS} \
