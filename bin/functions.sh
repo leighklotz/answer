79c9d49514b74ff5a2201c4f160a1e0f73ceed7c
Leigh Klotz <klotz@klotz.me>
2026-07-16 17:30:10 -0700

    Ensure hx subcommands return a zero exit status
    - Add explicit return 0 to provenance subcommand logic
    
    - Add explicit return 0 after model name selection in cache subcommand

--- bin/functions.sh
+++ bin/functions.sh
@@ -246,6 +246,7 @@ function hx() {
     # Handle provenance Subcommand
     if [[ "$1" == "provenance" ]]; then
         _hx_provenance $2
+        return 0
     # Handle Cache Subcommand
     elif [[ "$1" == "cache" ]]; then
         case "$2" in
@@ -327,6 +328,7 @@ function hx() {
 
         model)
             _get_model_name
+            return 0
         ;;
 
         *)
