e21695118c8f28432109a525fbf58c3754170739
Leigh Klotz <klotz@klotz.me>
2026-08-02 16:32:47 -0700

    checkdoc: output formatting

--- bin/checkdoc.sh
+++ bin/checkdoc.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd ~/wip/answer/doc/commands/
 
 for file in *.md.new
 do
-  echo "$file"
-  lx "${file}" | ask 'briefly look for llm-dreck (whole file wrapped in backquotes, LLM intro/outro) and output wither CLEAN or DRECK'
-  echo ""
+  old="${file%.new}"
+  echo "Analysis of $old -> $file"
+  lx "$old" "${file}" | dreck
+  echo "End of $old -> $file"
 done
