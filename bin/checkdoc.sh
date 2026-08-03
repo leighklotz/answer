d18966f303808bbe0a16f4c644df37ded6cdf31f
Leigh Klotz <klotz@klotz.me>
2026-08-02 17:01:17 -0700

    Update checkdoc.sh: improved output formatting and added 'answer' to pipeline

--- bin/checkdoc.sh
+++ bin/checkdoc.sh
@@ -5,7 +5,9 @@ cd ~/wip/answer/doc/commands/
 for file in *.md.new
 do
   old="${file%.new}"
-  echo "Analysis of $old -> $file"
-  lx "$old" "${file}" | dreck
-  echo "End of $old -> $file"
+  echo "=== Analysis of $old -> $file ==="
+  lx "$old" "${file}" | dreck | answer
+  echo "== End Analysis $old -> $file ==="
+  echo "----"
+  echo ""
 done
