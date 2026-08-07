a166eca27f8573e423b3a5b39fc87515b05938da
Leigh Klotz <klotz@klotz.me>
2026-08-07 12:16:27 -0700

    Refactor hx bootstrap function logic
    - Implement a case statement in 'hx()' for cleaner argument handling\n- Add chatty mode that enables and displays model information when no arguments are passed\n- Simplify error messages and remove redundant checks

--- bin/commands/hx-bootstrap.sh
+++ bin/commands/hx-bootstrap.sh
@@ -1,16 +1,23 @@
 # bootstrap hx
 # enable function only; once enabled, answer will override
 function hx () {
-    if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
-        echo "🦶ERROR: bash 4+ required (BASH_VERSION=${BASH_VERSION})." >&2
-        return 1 2>/dev/null
-    fi
-    if  [ "$1" == "enable" ]; then
-        source ~/wip/answer/bin/commands/enable
-    else
-        echo "usage: hx enable"
+    local arg="$1"
+
+    if [[ "${BASH_VERSINFO[0]}" -lt 4 ]]; then
+        echo "ERROR: bash 4+ required." >&2
         return 1
     fi
+
+    case "$arg" in
+        "")           # hx no arg, chatty
+            hx enable && echo -n "🦶hallux enabled: model=" && hx model
+            ;;
+        "enable")     # hx enable, quiet
+            source ~/wip/answer/bin/commands/enable
+            ;;
+        *)            # This handles any other input
+            echo "usage: hx [enable]"
+            return 1
+            ;;
+    esac
 }
-
-
